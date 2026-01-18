a98677243320a8c870d40897de28b2b3118afe35
Anton-Osip <osipchika@gmail.com>
2026-01-18 11:17:33 +0300

    feat: add storybooks settings for dark mode

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,25 +1,39 @@
-import "@fontsource/roboto/400.css";
-import "@fontsource/roboto/700.css";
-import "../src/styles/index.scss";
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/700.css'
+import '../src/styles/index.scss'
 
-import type { Preview } from "@storybook/react-vite";
-
+import type { Preview } from '@storybook/react-vite'
+import { themes } from 'storybook/theming'
 const preview: Preview = {
   parameters: {
+    docs: {
+      theme: themes.dark,
+    },
+    backgrounds: {
+      options: {
+        // 👇 Default options
+        dark: { name: 'Dark', value: '#000000' },
+
+        light: { name: 'Light', value: '#F7F9F2' },
+      },
+    },
     controls: {
       matchers: {
         color: /(background|color)$/i,
         date: /Date$/i,
       },
     },
-
+    layout: 'centered',
     a11y: {
       // 'todo' - show a11y violations in the test UI only
       // 'error' - fail CI on a11y violations
       // 'off' - skip a11y checks entirely
-      test: "todo",
+      test: 'todo',
     },
   },
-};
+  initialGlobals: {
+    backgrounds: { value: 'dark' },
+  },
+}
 
-export default preview;
+export default preview
